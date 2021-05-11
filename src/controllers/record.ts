130bb956c1c58ea86ac391f24376c5d67d719028
Tanja <tanjamiskovic1@gmail.com>
2021-05-11 16:35:39 +0100

    add error handling and typing to controllers

--- src/controllers/record.ts
+++ src/controllers/record.ts
@@ -1,5 +1,5 @@
+import express from 'express';
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 
 prisma
@@ -12,23 +12,36 @@ prisma
   });
 
 export async function getUserRecords(
-  req: {
-    body: { user_id: number };
-  },
-  res: any,
+  req: express.Request,
+  res: express.Response,
 ): Promise<void> {
   const user_id: number = req.body.user_id;
-  const records = await prisma.record.findMany({
-    where: {
-      user_id: user_id,
-    },
-  });
-  res.send(records);
+  try {
+    const records = await prisma.record.findMany({
+      where: {
+        user_id: user_id,
+      },
+    });
+    res.status(200);
+    res.send(records);
+  } catch (error) {
+    console.log(error);
+    res.end();
+  }
 }
 
-export async function addRecord(req: any, res: any): Promise<void> {
+export async function addRecord(
+  req: express.Request,
+  res: express.Response,
+): Promise<void> {
   const newRecord = req.body.record;
   console.log('newRecord', newRecord);
-  await prisma.record.create({ data: newRecord });
-  res.end();
+  try {
+    await prisma.record.create({ data: newRecord });
+    res.status(201);
+    res.end();
+  } catch (error) {
+    console.log(error);
+    res.end();
+  }
 }
